7c1e8b23b41b1113ea19565fb2cccae0fe98f2f2
happy52401 <1149815743@qq.com>
2019-09-17 15:54:05 +0800

    Initial commit

---
+++ src/main/java/com/lz/kuangchangvideo/KuangchangVideoApplication.java
@@ -0,0 +1,13 @@
+package com.lz.kuangchangvideo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuangchangVideoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KuangchangVideoApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/lz/kuangchangvideo/controller/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.lz.kuangchangvideo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/kc")
+public class HelloWorld {
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "hello";
+    }
+}
